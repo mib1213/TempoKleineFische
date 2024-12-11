bdb716b01bd5f1022457454f5e65ada947d0f313
omer <mirzaomer@outlook.de>
2024-12-12 00:21:05 +0100

    readme text added

--- simulation.py
+++ simulation.py
@@ -4,6 +4,6 @@ from parameters import NUMBER_OF_FISHES, WIN_STEPS, STEP
 winner, simulation_output = run_simulation(STEP, NUMBER_OF_FISHES, WIN_STEPS)
 
 for i in range(len(simulation_output['counter'])):
-    print("===============================", "<br>")
+    print("===============================")
     for key, value in simulation_output.items():
-        print(f"{key} = {value[i]} <br>")
+        print(f"{key} = {value[i]}")
